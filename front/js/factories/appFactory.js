fdb3b0703062bdf0c25b04729b35d931d44729ee
Dahakarful <tsar4le4grand@gmail.com>
2017-04-06 17:13:14 +0200

    Mise en place du module Google Maps d'AngularJS

---
+++ front/js/factories/appFactory.js
@@ -0,0 +1,14 @@
+wasaby.factory('appFactory', function ($http) {
+    
+    var buoysJson = "../../resources/buoys.json";
+
+    return {
+        loadBuoys: function(){
+            return $http({
+                method: 'GET',
+                url: buoysJson,
+                headers: {'Content-Type': 'application/json'}
+            });
+        },
+    }
+});
