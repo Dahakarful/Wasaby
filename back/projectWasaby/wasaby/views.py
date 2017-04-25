7b9d8742a4107094f9e2a15b10d56f9ae8b756c3
Dahakarful <tsar4le4grand@gmail.com>
2017-04-25 22:53:02 +0200

    requete de recherche vers elasticsearch

--- back/projectWasaby/wasaby/views.py
+++ back/projectWasaby/wasaby/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# from urllib import request
+from requests import request
+from django.http import HttpResponse
+import requests
 
-# Create your views here.
+url = "http://localhost:9200/buyos/product/1";
+
+def searchABuyo():
+    r = requests.get(url)
+    print(r.status_code, r.content)
+    # string = request.GET['name']
+    # return HttpResponse(string)
+
+searchABuyo()
