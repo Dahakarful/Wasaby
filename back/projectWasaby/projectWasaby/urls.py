982efc96a70f306807a60f85fc3e525fe74a52b4
Dahakarful <tsar4le4grand@gmail.com>
2017-05-01 18:26:55 +0200

    MAJ gitignore + ajout fonction indexation + fonction de recherche + ajout fichier json de test pour elasticsearch

--- back/projectWasaby/projectWasaby/urls.py
+++ back/projectWasaby/projectWasaby/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from wasaby import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^index/', views.indexBuoys),
+    url(r'^search/', views.searchABuoy),
 ]
--- back/projectWasaby/wasaby/views.py
+++ back/projectWasaby/wasaby/views.py
@@ -1,23 +1,29 @@
 import json
+import requests
 import elasticsearch
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-#url = "http://localhost:9200/buyos/product/1";
 url = 'http://localhost:9200/index/buoys'
 indexName = 'index'
 typeName = 'buoys'
-jsonPath = 'C:/Users/Ragonda/gitHub/Wasaby/front/resources'
+jsonPath = 'C:/Users/Ragonda/gitHub/Wasaby/back/projectWasaby/resources/buoys.json'
 
 es = elasticsearch.Elasticsearch()
 
-def indexBuoys():
+
+def indexBuoys(request):
+    print(request)
     nodes = json.load(open(jsonPath))
     print(len(nodes))
     for k, v in nodes.items():
         id = k
         res = es.index(index=indexName, doc_type=typeName, id=id, body=v)
+        print(res)
+    return HttpResponse("Number of created objects %d" % len(nodes))
 
-def searchABuyo():
-    r = requests.get(url)
-    print(r.status_code, r.content)
-    # string = request.GET['name']
-    # return HttpResponse(string)
+
+@csrf_exempt
+def searchABuoy(self):
+    data = self.request.data
+    print(data)
