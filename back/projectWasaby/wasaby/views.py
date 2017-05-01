fea06c2e9fe9f80fa6db216a04d4623725363c85
Dahakarful <tsar4le4grand@gmail.com>
2017-05-01 16:43:47 +0200

    Ajout fichier pour clevercloud + ajout fonction indexation d'un fichier json

--- back/projectWasaby/wasaby/views.py
+++ back/projectWasaby/wasaby/views.py
@@ -1,14 +1,23 @@
-# from urllib import request
-from requests import request
-from django.http import HttpResponse
-import requests
+import json
+import elasticsearch
 
-url = "http://localhost:9200/buyos/product/1";
+#url = "http://localhost:9200/buyos/product/1";
+url = 'http://localhost:9200/index/buoys'
+indexName = 'index'
+typeName = 'buoys'
+jsonPath = 'C:/Users/Ragonda/gitHub/Wasaby/front/resources'
+
+es = elasticsearch.Elasticsearch()
+
+def indexBuoys():
+    nodes = json.load(open(jsonPath))
+    print(len(nodes))
+    for k, v in nodes.items():
+        id = k
+        res = es.index(index=indexName, doc_type=typeName, id=id, body=v)
 
 def searchABuyo():
     r = requests.get(url)
     print(r.status_code, r.content)
     # string = request.GET['name']
     # return HttpResponse(string)
-
-searchABuyo()
